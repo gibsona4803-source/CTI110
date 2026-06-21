6a9c1d5814a5636b9d6329f7eeda6336b3458bf0
gibsona4803-source <gibsona4803@student.faytechcc.edu>
2026-06-21 14:02:41 -0400

    Add files via upload

---
+++ P1LAB1_ArianaBlackmon.py
@@ -0,0 +1,13 @@
+# Ariana Blackmon
+# 6/17/2026
+# Assignment Name: P1LAB1 - Welcome Message
+# Description: This program takes a user's first and last name as separate inputs and displays a welcoming message.
+
+# Get the first name from the user
+first_name = input("Enter your first name: ")
+
+# Get the last name from the user
+last_name = input("Enter your last name: ")
+
+# Display the welcome message using the entered variables
+print("Hello,", first_name, last_name + "! Welcome to CTI-110")
